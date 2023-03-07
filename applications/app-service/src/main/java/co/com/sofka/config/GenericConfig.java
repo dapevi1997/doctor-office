75a73fe6493e6575c67a24696adc98199d7b701e
dapevi1997 <dapevi97@gmail.com>
2023-03-07 16:31:06 -0500

    GenericConfig added

--- applications/app-service/src/main/java/co/com/sofka/config/GenericConfig.java
+++ applications/app-service/src/main/java/co/com/sofka/config/GenericConfig.java
@@ -1,10 +1,14 @@
 package co.com.sofka.config;
 
+import co.com.sofka.serializer.JSONMapper;
+import co.com.sofka.serializer.JSONMapperImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GenericConfig {
     @Bean
-    public
+    public JSONMapper jsonMapper(){
+        return new JSONMapperImpl();
+    }
 }
