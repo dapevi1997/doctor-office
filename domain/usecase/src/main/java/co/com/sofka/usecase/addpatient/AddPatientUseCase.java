d0bb580e4356f9a6ae2ce7a9139fc87907cbabdb
dapevi1997 <dapevi97@gmail.com>
2023-03-08 11:13:25 -0500

    Adapter type mongo created

--- domain/usecase/src/main/java/co/com/sofka/usecase/addpatient/AddPatientUseCase.java
+++ domain/usecase/src/main/java/co/com/sofka/usecase/addpatient/AddPatientUseCase.java
@@ -1,9 +1,40 @@
 package co.com.sofka.usecase.addpatient;
 
 
+import co.com.sofka.model.patient.Patient;
+import co.com.sofka.model.patient.generic.DomainEvent;
+import co.com.sofka.model.patient.values.ClinicHistory;
+import co.com.sofka.model.patient.values.Identity;
+import co.com.sofka.model.patient.values.PatientId;
 import co.com.sofka.usecase.generic.UseCaseForCommand;
 import co.com.sofka.usecase.generic.commands.AddPatientCommand;
+import co.com.sofka.usecase.generic.gateways.DomainEventRepository;
+import co.com.sofka.usecase.generic.gateways.EventBus;
 import lombok.RequiredArgsConstructor;
-@RequiredArgsConstructor
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+//@RequiredArgsConstructor
 public class AddPatientUseCase extends UseCaseForCommand<AddPatientCommand> {
+    private final DomainEventRepository repository;
+    private final EventBus bus;
+
+    public AddPatientUseCase(DomainEventRepository repository, EventBus bus){
+        this.repository = repository;
+        this.bus = bus;
+    }
+
+    @Override
+    public Flux<DomainEvent> apply(Mono<AddPatientCommand> addPatientCommandMono) {
+        return addPatientCommandMono.flatMapIterable(command->{
+            Patient patient = new Patient(PatientId.of(command.getId()),new Identity(command.getIdentity()),
+                    new ClinicHistory(command.getClinicHistory()));
+            return patient.getUncommittedChanges();
+        }).flatMap(event -> {
+            return repository.saveEvent(event);
+        }).map(event ->{
+            bus.publish(event);
+            return event;
+        });
+    }
 }
---
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/sofka/mongo/MongoRepositoryAdapter.java
@@ -0,0 +1,49 @@
+package co.com.sofka.mongo;
+
+import co.com.sofka.model.patient.generic.DomainEvent;
+import co.com.sofka.mongo.data.StoredEvent;
+import co.com.sofka.mongo.helper.AdapterOperations;
+import co.com.sofka.serializer.JSONMapper;
+import co.com.sofka.usecase.generic.gateways.DomainEventRepository;
+import org.reactivecommons.utils.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Criteria;
+
+import java.util.Comparator;
+import java.util.Date;
+
+@Repository
+public class MongoRepositoryAdapter implements DomainEventRepository
+{
+    private final ReactiveMongoTemplate template;
+    private final JSONMapper eventSerializer;
+
+    public MongoRepositoryAdapter(ReactiveMongoTemplate template, JSONMapper eventSerializer) {
+        this.template = template;
+        this.eventSerializer = eventSerializer;
+    }
+
+    @Override
+    public Flux<DomainEvent> findById(String aggregateId) {
+        var query = new Query(Criteria.where("aggregateRootId").is(aggregateId));
+        return template.find(query, StoredEvent.class)
+                .sort(Comparator.comparing(event -> event.getOccurredOn()))
+                .map(storeEvent -> storeEvent.deserializeEvent(eventSerializer));
+    }
+
+    @Override
+    public Mono<DomainEvent> saveEvent(DomainEvent event) {
+        StoredEvent eventStored = new StoredEvent();
+        eventStored.setAggregateRootId(event.aggregateRootId());
+        eventStored.setTypeName(event.getClass().getTypeName());
+        eventStored.setOccurredOn(new Date());
+        eventStored.setEventBody(StoredEvent.wrapEvent(event, eventSerializer));
+        return template.save(eventStored)
+                .map(storeEvent -> storeEvent.deserializeEvent(eventSerializer));
+    }
+}
