a65c4a7141c1db1666564e11212431c5737775c2
dapevi1997 <dapevi97@gmail.com>
2023-03-15 21:41:04 -0500

    Get citations updated

--- domain/usecase/src/main/java/co/com/sofka/usecase/getcitations/GetCitationUseCase.java
+++ domain/usecase/src/main/java/co/com/sofka/usecase/getcitations/GetCitationUseCase.java
@@ -2,6 +2,9 @@ package co.com.sofka.usecase.getcitations;
 
 
 import co.com.sofka.model.patient.generic.DomainEvent;
+import co.com.sofka.model.week.Week;
+import co.com.sofka.model.week.events.CitationAdded;
+import co.com.sofka.model.week.values.WeekId;
 import co.com.sofka.usecase.generic.UseCaseForCommand;
 import co.com.sofka.usecase.generic.commands.GetCitationCommand;
 import co.com.sofka.usecase.generic.gateways.DomainEventRepository;
@@ -20,9 +23,25 @@ public class GetCitationUseCase extends UseCaseForCommand<GetCitationCommand> {
 
     @Override
     public Flux<DomainEvent> apply(Mono<GetCitationCommand> getCitationCommandMono) {
+        return getCitationCommandMono.flatMapMany(command -> {
+            return repository.findById(command.getIdWeek())
+                     .collectList()
+                     .flatMapIterable(events -> {
+                         Week week = Week.from(WeekId.of(command.getIdWeek()), events);
+                         return week.getCitations();
+                     }).map(citation -> {
+                         return new CitationAdded(citation.identity().value(),citation.getInfomation().value(),citation.getCitationState().value(),
+                                 citation.getPatientId().value(),citation.getWeekId().value());
+                    });
+        });
+
+    }
+
+/*    @Override
+    public Flux<DomainEvent> apply(Mono<GetCitationCommand> getCitationCommandMono) {
         Flux<DomainEvent> domainEventFlux = getCitationCommandMono.flatMapMany(command -> {
             return repository.findCitation(command.getIdWeek());
         });
         return domainEventFlux;
-    }
+    }*/
 }
